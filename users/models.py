dd6d5da123f9a0241441baff2095c4ec18ec4e77
Anushree Vinod <anushreevinod10@gmail.com>
2024-11-10 23:50:00 +0000

    Create Customer model

--- users/models.py
+++ users/models.py
@@ -2,3 +2,7 @@ from django.db import models
 
 class ShowProducer(models.Model):
     email = models.EmailField(unique=True)
+
+
+class Customer(models.Model):
+    loyaltyPoints = models.IntegerField()
