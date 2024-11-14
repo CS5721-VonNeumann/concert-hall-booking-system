d1fadf35bdd1dcc0e30ad6a5feabccbdb4b91889
Anushree Vinod <anushreevinod10@gmail.com>
2024-11-14 20:36:09 +0000

    fetch customer using middleware in book ticket

--- ticket_manager/views.py
+++ ticket_manager/views.py
@@ -1,24 +1,26 @@
-from django.core import serializers
-from django.forms.models import model_to_dict
-from django.http import HttpRequest, JsonResponse
 import json
 
-from .services import is_seat_available, create_ticket
+from django.http import HttpRequest, JsonResponse
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 
+from users.middleware import get_current_user
+from users.models import Customer
+from .services import return_available_seats, create_ticket
+
+@api_view(["POST"])
+@permission_classes([IsAuthenticated])
 def bookTickets(request: HttpRequest):
-    if request.method == 'POST':
-        data = json.loads(request.body)
-        show_id = data.get('show_id')
-        seats = data.get('seats')  # Array of seat IDs or numbers
-        customer_id = data.get('customer_id')
+    customer = Customer.objects.get(user=get_current_user())
 
-        if seat_objs := is_seat_available(show_id, seats):
-            # TODO: payment gateway
-            # pass
-            if tickets := create_ticket(customer_id, show_id, seat_objs):
-                return JsonResponse({"ticket_ids": list(tickets)})
-            return JsonResponse({"error": "Something went wrong creating ticketse."}, status=404)
+    data = json.loads(request.body)
+    show_id = data.get('show_id')
+    seats = data.get('seats')  # Array of seat IDs or numbers
 
-        return JsonResponse({"error": "Seat not available."}, status=404)
-        
-    return JsonResponse({"error": "Invalid request method."}, status=405)
+    if seat_objs := return_available_seats(show_id, seats):
+        # TODO: payment gateway
+        if tickets := create_ticket(customer, show_id, seat_objs):
+            return JsonResponse({"ticket_ids": list(tickets)})
+        return JsonResponse({"error": "Something went wrong creating tickets."}, status=400)
+
+    return JsonResponse({"error": "Seat not available."}, status=404)
