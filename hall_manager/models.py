559a44a8bff5c6ad10e4a848d7a699059d74b8b0
Mahika Jaguste <mahika.for.me@gmail.com>
2024-11-18 23:01:27 +0000

    seat type and concrete seat types added, apis to add seats to hall and change seat types

--- hall_manager/models.py
+++ hall_manager/models.py
@@ -1,4 +1,10 @@
 from django.db import models
+from .seattypes import SeatTypeEnum, SeatType, RegularSeatType, PremiumSeatType
+
+SEAT_TYPE_CLASSES = {
+    SeatTypeEnum.REGULAR.name: RegularSeatType,
+    SeatTypeEnum.PREMIUM.name: PremiumSeatType,
+}
 
 class Slot(models.Model):
     TIMING_CHOICES = [
@@ -61,3 +67,17 @@ class HallSupportsSlot(models.Model):
 class Seat(models.Model):
     seat_number = models.PositiveIntegerField()
     hall = models.ForeignKey(Hall, on_delete=models.CASCADE, null=True, related_name="seats")
+
+    SEAT_TYPE_CHOICES = [(seat_type.name, seat_type.name) for seat_type in SeatTypeEnum]
+    seat_type = models.CharField(max_length=10, choices=SEAT_TYPE_CHOICES, default=SeatTypeEnum.REGULAR.name)
+
+    def get_seat_type_instance(self):
+        seat_type = SEAT_TYPE_CLASSES.get(self.seat_type)
+        if seat_type:
+            return seat_type()
+        raise ValueError(f"Unknown status: {self.status}")
+    
+    def get_seat_price(self):
+        seat_type_instance: SeatType =  self.get_seat_type_instance()
+        return seat_type_instance.get_price()
+       
--- hall_manager/views.py
+++ hall_manager/views.py
@@ -1,9 +1,15 @@
 import json
 from django.http import JsonResponse, HttpRequest
 from django.shortcuts import get_object_or_404
-from .models import Hall, Venue, Slot, Category, HallSupportsSlot, HallSupportsCategory
+from .models import Hall, Venue, Slot, Category, HallSupportsSlot, HallSupportsCategory, Seat
+from .seattypes import SeatTypeEnum
 from show_manager.models import Show
 from django.forms.models import model_to_dict
+from django.db.models import F
+from users.middleware import get_current_user
+from rest_framework.decorators import permission_classes
+from rest_framework.decorators import api_view
+from rest_framework.permissions import IsAuthenticated
 
 def create_hall(request: HttpRequest):
     if request.method == 'POST':
@@ -146,3 +152,89 @@ def get_halls(request: HttpRequest):
             print(e)
     
     return JsonResponse({"error": "Invalid request method."}, status=405)
+
+@api_view(["POST"])
+@permission_classes([IsAuthenticated])
+def add_seats_to_hall(request: HttpRequest):
+    user = get_current_user()
+    if not user.is_superuser:
+        return JsonResponse({"error": "Permission denied"}, status=403)
+    
+    body = json.loads(request.body)
+
+    hall_id = body.get('hall_id')
+    hall = get_object_or_404(Hall, id=hall_id)
+
+    seat_numbers = body.get('seat_numbers')
+    
+    if not hall_id or not seat_numbers:
+        return JsonResponse({"error": "hall_id and seat_numbers are required"}, status=400)
+
+    existing_seats = set(Seat.objects.filter(hall=hall).values_list("seat_number", flat=True))
+    print(existing_seats)
+    new_seat_numbers = [num for num in seat_numbers if num not in existing_seats]
+
+    Seat.objects.bulk_create([
+        Seat(seat_number=num, hall=hall) for num in new_seat_numbers
+    ])
+
+    if len(new_seat_numbers):
+        hall.hall_capacity = F('hall_capacity') + len(new_seat_numbers)
+        hall.save()
+
+    return JsonResponse({
+        "message": f"Added {len(new_seat_numbers)} seats to hall {hall.hall_name}",
+        "existing_seats_skipped": len(seat_numbers) - len(new_seat_numbers),
+    }, status=200)
+
+        
+
+@api_view(["POST"])
+@permission_classes([IsAuthenticated])
+def change_seat_type(request: HttpRequest):
+    user = get_current_user()
+    if not user.is_superuser:
+        return JsonResponse({"error": "Permission denied"}, status=403)
+    
+    body = json.loads(request.body)
+    hall_id = body.get("hall_id")
+    hall = get_object_or_404(Hall, id=hall_id)
+
+    seat_updates = body.get("seat_updates")  # Expecting a list of {"seat_number": int, "seat_type": str}
+
+    if not hall_id or not seat_updates:
+        return JsonResponse({"error": "hall_id and seat_updates are required"}, status=400)
+
+    seat_types = {i.name: i.value for i in SeatTypeEnum}
+    invalid_updates = []
+    updated_count = 0
+
+    for update in seat_updates:
+        seat_number = update.get("seat_number")
+        seat_type = update.get("seat_type")
+
+        if not seat_number:
+            update['reason'] = "Invalid seat number"
+            invalid_updates.append(update)
+            continue
+
+        if not seat_type or seat_type not in seat_types:
+            update['reason'] = "Invalid seat type"
+            invalid_updates.append(update)
+            continue
+
+        seat = Seat.objects.filter(hall=hall, seat_number=seat_number).first()
+        if seat:
+            seat.seat_type = seat_type
+            seat.save()
+            updated_count += 1
+        else:
+            update['reason'] = "Seat does not exist"
+            invalid_updates.append(update)
+
+    return JsonResponse({
+        "message": f"Updated {updated_count} seats in hall {hall.hall_name}",
+        "invalid_updates": invalid_updates,
+    }, status=200)
+    
+        
