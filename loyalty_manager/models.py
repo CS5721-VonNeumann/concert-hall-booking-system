a2e27ad181f64f4bd3f74868c6ec5c16ea363454
adarshajit <adarshajit@gmail.com>
2024-11-20 10:11:50 +0000

    feat: add model for customer loyalty

--- loyalty_manager/models.py
+++ loyalty_manager/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.utils.timezone import now
 
-# Create your models here.
+from users.models import Customer
+from loyalty_manager.decorators import MembershipLoyaltyDecorator, NewCustomerLoyaltyDecorator, RegularLoyalty
+
+class CustomerLoyalty(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name="customerloyalties")
+    loyalty_points = models.IntegerField(default=0)
+
+    def calculate_loyalty_points(self):
+        # customers with expired memberships will have zero loyalty points
+        if self.expiry < now():
+            self.loyalty_points = 0
+            self.save()
+            return 0
+
+        regular_loyalty = RegularLoyalty()
+
+        loyalty_with_membership = MembershipLoyaltyDecorator(regular_loyalty, self.membership_type)
+
+        total_loyalty = NewCustomerLoyaltyDecorator(loyalty_with_membership, self.customer)
+
+        return total_loyalty.get_loyalty_points()
