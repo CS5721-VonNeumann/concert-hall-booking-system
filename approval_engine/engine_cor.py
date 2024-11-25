201c4e8ee9831871a083c8cae9c6d1672bbd2806
Mahika Jaguste <mahika.for.me@gmail.com>
2024-11-25 22:33:53 +0000

    added BaseTaskWithRetry class to be used by all shared tasks which has retry with exponential backoff, setup DLQ in celery rabbitmq and after final failure Reejct the task to deliver it to DLQ

--- approval_engine/engine_cor.py
+++ approval_engine/engine_cor.py
@@ -61,18 +61,16 @@ class ApprovalEngine:
     def handle_show_request(self):
         """
         Handles the show request by validating it through the chain.
+        If error occurs, the shared task will retry 3 times and send to DLQ on further failure
         """
-        try:
-            if not isinstance(self.show.get_status_instance(), PendingStatus):
-                print("Request not in pending status")
-                return
+        if not isinstance(self.show.get_status_instance(), PendingStatus):
+            print("Request not in pending status")
+            return
 
-            is_valid, message = self.validation_chain.handle(self.show)
+        is_valid, message = self.validation_chain.handle(self.show)
 
-            if is_valid:
-                self.show.schedule()
-            else:
-                self.show.reject(message)
-        except Exception as e:
-            print(f"Error processing show request: {e}")
-            self.show.reject("Error in validating request")
+        if is_valid:
+            self.show.schedule()
+        else:
+            self.show.reject(message)
+        
